610c553fff13e5b9ca06435f87b4e732476e00b3
zhyjll <705426434@qq.com>
2014-03-16 15:47:58 +0800

    双ADC同步规则通道模式，ADC1-通道1,2；ADC2-通道10,14.

--- test/test/src/adc.c
+++ test/test/src/adc.c
@@ -5,7 +5,9 @@
  *      Author: zhy_jll
  */
 #include "adc.h"
-__IO uint16_t ADCConvertedValue[3];
+__IO uint32_t ADCConvertedValue[2];
+__IO uint32_t ADC_DualConvertedValueTab[16];
+#define ADC1_DR_Address    ((uint32_t)0x4001244C)
 
 void Adc1_Init(void)
 {
@@ -15,29 +17,36 @@ void Adc1_Init(void)
 
 void ADC1_GPIO_Config(void)
 {
+
 	 GPIO_InitTypeDef  GPIO_InitStructure;
 	 RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1 ,ENABLE);
-	 RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_ADC1,ENABLE);
+	 RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_ADC1|RCC_APB2Periph_ADC2|RCC_APB2Periph_GPIOC,ENABLE);
 	 RCC_ADCCLKConfig(RCC_PCLK2_Div8);
+
 	 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
-	 GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	 GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;
 	 GPIO_Init(GPIOA,&GPIO_InitStructure);
+
+	 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	 GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_0 | GPIO_Pin_4;
+	 GPIO_Init(GPIOC,&GPIO_InitStructure);
 
 }
 
 void ADC1_Mode_config()
 {
+
 	DMA_InitTypeDef DMA_InitStructure;
 	DMA_DeInit(DMA1_Channel1);
 	DMA_InitStructure.DMA_PeripheralBaseAddr = (u32)&ADC1->DR;
-	DMA_InitStructure.DMA_MemoryBaseAddr =(u32)&ADCConvertedValue;
+	DMA_InitStructure.DMA_MemoryBaseAddr = (u32)ADCConvertedValue;
 	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
-	DMA_InitStructure.DMA_BufferSize = 3;
+	DMA_InitStructure.DMA_BufferSize = 2;
 	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
 	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
-	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Word;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Word;
+	DMA_InitStructure.DMA_Mode =  DMA_Mode_Circular;
 	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
 	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
 	DMA_Init(DMA1_Channel1,&DMA_InitStructure);
@@ -45,26 +54,44 @@ void ADC1_Mode_config()
 
 	ADC_InitTypeDef ADC_InitStructure;
 	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
-	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+	ADC_InitStructure.ADC_Mode = ADC_Mode_RegSimult;
 	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
 	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
 	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None ;
-	ADC_InitStructure.ADC_NbrOfChannel = 3;
+	ADC_InitStructure.ADC_NbrOfChannel = 2;
 	ADC_Init(ADC1,&ADC_InitStructure);
 
 	ADC_RegularChannelConfig(ADC1,ADC_Channel_1,1,ADC_SampleTime_55Cycles5);
 	ADC_RegularChannelConfig(ADC1,ADC_Channel_2,2,ADC_SampleTime_55Cycles5);
-	ADC_RegularChannelConfig(ADC1,ADC_Channel_3,3,ADC_SampleTime_55Cycles5);
-
 
 	ADC_DMACmd(ADC1,ENABLE);
+
+/*   INIT ADC2*/
+	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+	ADC_InitStructure.ADC_Mode = ADC_Mode_RegSimult;
+	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_NbrOfChannel = 2;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	ADC_Init(ADC2,&ADC_InitStructure);
+
+	ADC_RegularChannelConfig(ADC2,ADC_Channel_10,1,ADC_SampleTime_55Cycles5);
+	ADC_RegularChannelConfig(ADC2,ADC_Channel_14,2,ADC_SampleTime_55Cycles5);
+
+	ADC_ExternalTrigConvCmd(ADC2, ENABLE);
+
+
 	ADC_Cmd(ADC1,ENABLE);
-
-
 	ADC_ResetCalibration(ADC1);
 	while(ADC_GetResetCalibrationStatus(ADC1));
 	ADC_StartCalibration(ADC1);
 	while(ADC_GetCalibrationStatus(ADC1));
+
+	ADC_Cmd(ADC2,ENABLE);
+	ADC_ResetCalibration(ADC2);
+	while(ADC_GetResetCalibrationStatus(ADC2));
+	ADC_StartCalibration(ADC2);
+	while(ADC_GetCalibrationStatus(ADC2));
 	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
 }
 
--- test/test/src/main.c
+++ test/test/src/main.c
@@ -15,7 +15,8 @@
 #include "stdio.h"
 #include "unistd.h"
 
-extern __IO uint16_t ADCConvertedValue[];
+extern __IO uint32_t ADCConvertedValue[2];
+extern __IO uint32_t ADC_DualConvertedValueTab[16];
 
 
 #define  Channel_Number 2 // 通道数的宏定义
@@ -25,7 +26,8 @@ float AD_Analog_Value[Channel_Number];					//AD模拟电压值
  int main(void)
  {
 	 float temp,Ad_Analog_Value[3];
-	 u16 cache[3];
+	 u16 cache[4];
+	 u32 cach[16];
 	 u8 i;
 	 u16 k,m;
 	SystemInit(); 			 //系统时钟初始化为72M	  SYSCLK_FREQ_72MHz
@@ -45,21 +47,24 @@ float AD_Analog_Value[Channel_Number];					//AD模拟电压值
 	LCD_ShowString(30,110,(const u8 *)"2014/02/22");
 
 	POINT_COLOR=BLUE;//设置字体为蓝色
-	LCD_ShowString(60,130,(const u8 *)"ADC_CH0_VAL:");
-	LCD_ShowString(60,150,(const u8 *)"ADC_CH0_VOL:0.000V");
-	LCD_ShowString(60,170,(const u8 *)"ADC_CH0_VAL:");
-	LCD_ShowString(60,190,(const u8 *)"ADC_CH0_VOL:0.000V");
-	LCD_ShowString(60,210,(const u8 *)"ADC_CH0_VAL:");
-	LCD_ShowString(60,230,(const u8 *)"ADC_CH0_VOL:0.000V");
-	Delay_us(2000);//这个地方特定添加延时 不然显示屏没有显示
+	LCD_ShowString(60,130,(const u8 *)"ADC_CH1_VAL:");
+	LCD_ShowString(60,150,(const u8 *)"ADC_CH1_VOL:0.000V");
+	LCD_ShowString(60,170,(const u8 *)"ADC_CH10_VAL:");
+	LCD_ShowString(60,190,(const u8 *)"ADC_CH10_VOL:0.000V");
+	LCD_ShowString(60,210,(const u8 *)"ADC_CH2_VAL:");
+	LCD_ShowString(60,230,(const u8 *)"ADC_CH2_VOL:0.000V");
+
+	Delay_us(20000);//这个地方特定添加延时 不然显示屏没有显示
+
 
 		while(1)
 	{
-			ADC_SoftwareStartConvCmd(ADC1, ENABLE);//软件启动AD转换
-//			while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));//等待转换结束
-		    while(DMA_GetFlagStatus(DMA1_FLAG_TC1)==RESET);//等待传输完成否则第一位数据容易丢失
-			DMA_ClearFlag(DMA1_FLAG_TC1); //清除通道 1 传输完成标志
-			ADC_SoftwareStartConvCmd(ADC1, DISABLE);//软件启动AD转换
+			  ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+
+			  /* Test on DMA1 channel1 transfer complete flag */
+			  while(!DMA_GetFlagStatus(DMA1_FLAG_TC1));
+			  /* Clear DMA1 channel1 transfer complete flag */
+			  DMA_ClearFlag(DMA1_FLAG_TC1);
 
 
 			Delay_us(500000);
@@ -70,10 +75,20 @@ float AD_Analog_Value[Channel_Number];					//AD模拟电压值
      		LED2(ON);			 // 也可以使用	  GPIO_ResetBits(GPIOD,GPIO_Pin_2) ;
 
 
-     		for(i=0;i<3;i++)
+     		for(i=0;i<2;i++)
      		{
-     			cache[i]=ADCConvertedValue[i];
+     			cach[i] = ADCConvertedValue[i];
      		}
+
+     		cache[0] = cach[0] & 0x0000ffff;    //ADC1-PA1
+     		cache[1] = (cach[0]>>16) & 0x0000ffff;  //ADC1-PA2
+     		cache[2] = cach[1] & 0x0000ffff;       //ADC2-PC0
+     		cache[3] = (cach[1]>>16) & 0x0000ffff;   //ADC2-PC4
+
+     		Send_Num(cache[0]);
+     		Send_Num(cache[1]);
+     		Send_Num(cache[2]);
+     		Send_Num(cache[3]);
 
      		k=cache[0];
      		LCD_ShowNum(156,130,k,4,16);
@@ -86,19 +101,19 @@ float AD_Analog_Value[Channel_Number];					//AD模拟电压值
     		k=temp;
     		LCD_ShowNum(172,150,k,3,16);
 
-    		Delay_us(500);//这个延时也必须保证足够大，不然显示屏依然不工作
+    		Delay_us(5000);//这个延时也必须保证足够大，不然显示屏依然不工作
     		m=cache[1];
-     		LCD_ShowNum(156,170,m,4,16);
+     		LCD_ShowNum(164,170,m,4,16);
      		temp=(float)m/4095*3.3;
      		m=temp;
      		Send_Num(m);
-     		LCD_ShowNum(156,190,m,1,16);//显示电压值
+     		LCD_ShowNum(164,190,m,1,16);//显示电压值
     		temp=temp-m;
     		temp=temp*1000;
     		m=temp;
-    		LCD_ShowNum(172,190,m,3,16);
+    		LCD_ShowNum(180,190,m,3,16);
+    		Delay_us(5000);//这个延时也必须保证足够大，不然显示屏依然不工作
 
-    		Delay_us(500);//这个延时也必须保证足够大，不然显示屏依然不工作
     		m=cache[2];
     		LCD_ShowNum(156,210,m,4,16);
     		temp=(float)m/4095*3.3;
@@ -109,7 +124,7 @@ float AD_Analog_Value[Channel_Number];					//AD模拟电压值
     		temp=temp*1000;
     		m=temp;
     		LCD_ShowNum(172,230,m,3,16);
-    		Delay_us(500);//这个延时也必须保证足够大，不然显示屏依然不工作
+    		Delay_us(5000);//这个延时也必须保证足够大，不然显示屏依然不工作
 
 
 	}
